f9dd48d6a721b9161f95a1f66d463f432fd0db9e
Bryan-Unic <juangomez94395@gmail.com>
2025-05-28 05:22:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY NICOL AREVALO!'],
+			strings: [ 'HAPPY', 'BIRTHDAY NICÓL ARÉVALO!'],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
